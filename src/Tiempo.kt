e3e450afa2460df92697ad2447745bda90be4c3a
Sara S Camilleri <ssancam391@g.educaand.es>
2024-01-31 11:11:50 +0100

    mismo problemas de actualizacion horas

--- src/Tiempo.kt
+++ src/Tiempo.kt
@@ -1,19 +1,15 @@
 class Tiempo(horas: Int, minutos: Int, segundos: Int){
 
+    private var horas: Int
+    private var minutos: Int
+    private var segundos: Int
+
     //GETTER Y SETTERS - PROPIEDADES DE CLASE
-    private var horas: Int = 0
-        private set
-
-    private var minutos: Int = 0
-        private set
-
-    private var segundos: Int = 0
-        private set
-
+    //redundantes en este caso
     init {
-    require(horas in 1..23) { "Datos de horas erróneo. "}
-    require(minutos in 1 ..59) { "Datos de minutos erróneos." }
-    require(segundos in 1..59) { "Datos de segundos erróneos. "}
+    require(horas in 1..24) { "Datos de horas erróneo. "}
+    require(minutos in 1 ..60) { "Datos de minutos erróneos." }
+    require(segundos in 1..60) { "Datos de segundos erróneos. "}
         this.horas = horas
         this.minutos = minutos
         this.segundos = segundos
