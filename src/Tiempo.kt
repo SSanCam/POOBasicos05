333a3a3934fe9e7e66e756bb6bfcf826561796fe
Sara S Camilleri <ssancam391@g.educaand.es>
2024-01-31 10:15:58 +0100

    constructor principal

---
+++ src/Tiempo.kt
@@ -0,0 +1,30 @@
+class Tiempo(horas: Int, minutos: Int, segundos: Int) {
+
+    //GETTER Y SETTERS - PROPIEDADES DE CLASE
+    private var horas: Int = 0
+        get() = field
+        private set
+
+    private var minutos: Int = 0
+        get() = field
+        private set
+    private var segundos: Int = 0
+        get() = field
+        private set
+
+    init {
+    require(horas in 1..23) { "Datos de horas erróneo. "}
+    require(minutos in 1..59) { "Datos de minutos erróneos." }
+    require(segundos in 1..59) { "Datos erróneos. "}
+
+        this.horas = horas
+        this.minutos = minutos
+        this.segundos = segundos
+    }
+
+
+    override fun toString(): String {
+        return ("${horas.toString().padStart(2, '0')}h ${minutos.toString().padStart(2, '0')}m ${segundos.toString().padStart(2, '0')}")
+    }
+
+}
