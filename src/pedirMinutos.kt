815871c079bd4282578ccbd05b0f7b3fab6f080c
Sara S Camilleri <ssancam391@g.educaand.es>
2024-02-01 08:19:41 +0100

    doc. pedirMinutos

--- src/pedirMinutos.kt
+++ src/pedirMinutos.kt
@@ -1,13 +1,20 @@
+/**
+ * Solicita al usuario ingresar los minutos y valida que estén en el rango de 1 a 59.
+ * @return Los minutos ingresados por el usuario.
+ */
 fun pedirMinutos(): Int {
     try {
         println("Introduce los minutos: ")
         var minutosIntro = readln()
+
+        // Validar que la entrada sea un número y esté en el rango de 1 a 59.
         while (minutosIntro.toInt() !in 1..59) {
             println("ERROR. Inténtalo de nuevo: ")
             minutosIntro = readln()
         }
         return minutosIntro.toInt()
     } catch (e: NumberFormatException) {
+        // Si ocurre una excepción al convertir a entero, retornar 0.
         return 0
     }
 }
